8a30149d843a92ae329b12050bd3b270aadfb717
Connor_w <connorw@unm.edu>
2024-11-19 14:54:42 -0700

    Update eng.py

--- eng.py
+++ eng.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 st.title("CSV Data Visualization App")
 
 # File uploader for CSV
-uploaded_file = st.file_uploader("AQI_dat", type=["csv"])
+uploaded_file = st.file_uploader("AQI_datv2", type=["csv"])
 
 if uploaded_file is not None:
     # Read the CSV file
